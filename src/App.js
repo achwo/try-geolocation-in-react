5f62e71200146feb7a7ed3b49f82dc0624ed5087
Felix Jensen <felix.jensen@mindmatters.de>
2022-05-10 13:18:00 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import { usePosition } from "./geohook";
 import './App.css';
+import { useEffect, useState } from "react";
+
+const permissionsAvailable = "permissions" in navigator;
 
 function App() {
+  const { position, geoAvailable, error } = usePosition({ watch: true });
+  const [ timer, setTimer ] = useState(0);
+  useEffect(() => {
+  const interval = setInterval(() => {
+    console.log('This will run every second!', timer);
+    setTimer(timer + 1);
+  }, 1000);
+  return () => clearInterval(interval);
+}, [timer]);
+  // const [ result, setResult] = useState(null);
+
+  // useEffect(() => {
+  //   if (!permissionsAvailable) {
+  //     console.log("no permissions api");
+  //     return
+  //   }
+
+  //   navigator.permissions.query({ name: "geolocation" }).then((r) => {
+  //     setResult(r);
+  //   });
+
+  // }, [navigator.permissions, setResult]);
+
+  // if (result) {
+  //   console.log("result", result);
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       { result.state }
+
+  //     </header>
+  //   </div>
+  // );
+  // }
+
+  //   return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       Position wird geladen
+  //     </header>
+  //   </div>
+  //   );
+
+  // if (!"permissions" in navigator) {
+  //   return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       Keine Permissions verfügbar
+  //     </header>
+  //   </div>
+  //   );
+  // }
+
+
+  if (!geoAvailable) {
+    return (
+    <div className="App">
+      <header className="App-header">
+        Kein Geo verfügbar
+      </header>
+    </div>
+    );
+  }
+  if (error) {
+    console.error(error);
+    return (
+    <div className="App">
+      <header className="App-header">
+      { error.message }
+      </header>
+    </div>
+    );
+  }
+  if (!position) {
+    return (
+    <div className="App">
+      <header className="App-header">
+        Position wird geladen
+      </header>
+    </div>
+    );
+  }
+  const { coords: { latitude, longitude }, timestamp} = position;
+
+  const url = `https://duckduckgo.com/?q=lat%3A+${latitude}++lng%3A+${longitude}&ia=web&iaxm=maps`
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href={url} target="_blank" rel="noreferrer">
+          lat: {  position?.coords.latitude } lng: {  position?.coords.longitude }
         </a>
+        <p>{timestamp}</p>
+        <p>{new Date(timestamp).toLocaleString() }</p>
+        <p>{new Date().toString() }</p>
+    <p>{timer}</p>
       </header>
     </div>
   );
---
+++ src/geohook.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+const geo = navigator.geolocation;
+const geoAvailable = 'geolocation' in navigator;
+
+const options = {
+  enableHighAccuracy: true
+}
+
+export function usePosition({ watch = false }) {
+  const [position, setPosition] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!geoAvailable) {
+      setError(new Error("Browser kennt geolocation nicht"));
+      return;
+    }
+
+    const onChange = (p) => {
+      console.log("position changed", p);
+      console.log("uhr", new Date(p.timestamp))
+      setPosition(p);
+    }
+
+    const onError = (err) => {
+      setError(err);
+    }
+
+    if (!watch) {
+      geo.getCurrentPosition(onChange, onError, options);
+    }
+
+    const watchId = geo.watchPosition(onChange, onError, options);
+    return () => geo.clearWatch(watchId);
+  }, [geo, geoAvailable, setPosition]);
+
+
+  // {
+  //   position,
+  //   error,
+  //   prompted,
+  //   isAllowedToUsePosition,
+    // isGeoAvailable => !('geolocation' in navigator)
+  // }
+
+  return { position, geoAvailable, error };
+}
